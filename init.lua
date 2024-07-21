8a8ef30646546bef1f7d5515b409aaf1bffccd9e
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-22 05:56:43 +0700

    add my old keybind

--- init.lua
+++ init.lua
@@ -910,3 +910,125 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+--
+--
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+vim.opt.guifont = 'sarabun'
+
+-- Key mappings
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set({ 'n', 'v' }, 'H', '^')
+vim.keymap.set({ 'n', 'v' }, 'L', '$')
+
+-- Clipboard setting
+vim.opt.clipboard = 'unnamed'
+
+-- Thai character mappings
+local thai_mappings = {
+  ['ๅ'] = '1',
+  ['ภ'] = '4',
+  ['ถ'] = '5',
+  ['ุ'] = '6',
+  ['ึ'] = '7',
+  ['ค'] = '8',
+  ['ต'] = '9',
+  ['จ'] = '0',
+  ['ข'] = '-',
+  ['ช'] = '=',
+  ['\\%'] = '~',
+  ['๑'] = '@',
+  ['๒'] = '#',
+  ['๓'] = '$',
+  ['๔'] = '%',
+  ['ู'] = '^',
+  ['฿'] = '&',
+  ['๕'] = '*',
+  ['๖'] = '(',
+  ['๗'] = ')',
+  ['๘'] = '_',
+  ['๙'] = '+',
+  ['ๆ'] = 'q',
+  ['ไ'] = 'w',
+  ['ำ'] = 'e',
+  ['พ'] = 'r',
+  ['ะ'] = 't',
+  ['ั'] = 'y',
+  ['ี'] = 'u',
+  ['ร'] = 'i',
+  ['น'] = 'o',
+  ['ย'] = 'p',
+  ['บ'] = '[',
+  ['ล'] = ']',
+  ['ฟ'] = 'a',
+  ['ห'] = 's',
+  ['ก'] = 'd',
+  ['ด'] = 'f',
+  ['เ'] = 'g',
+  ['้'] = 'h',
+  ['่'] = 'j',
+  ['า'] = 'k',
+  ['ส'] = 'l',
+  ['ว'] = ';',
+  ['ง'] = "'",
+  ['ผ'] = 'z',
+  ['ป'] = 'x',
+  ['แ'] = 'c',
+  ['อ'] = 'v',
+  ['ิ'] = 'b',
+  ['ื'] = 'n',
+  ['ท'] = 'm',
+  ['ม'] = ',',
+  ['ใ'] = '.',
+  ['ฝ'] = '/',
+  ['๐'] = 'Q',
+  ['ฎ'] = 'E',
+  ['ฑ'] = 'R',
+  ['ธ'] = 'T',
+  ['ํ'] = 'Y',
+  ['๊'] = 'U',
+  ['ณ'] = 'I',
+  ['ฯ'] = 'O',
+  ['ญ'] = 'P',
+  ['ฐ'] = '{',
+  ['ฤ'] = 'A',
+  ['ฆ'] = 'S',
+  ['ฏ'] = 'D',
+  ['โ'] = 'F',
+  ['ฌ'] = 'G',
+  ['็'] = 'H',
+  ['๋'] = 'J',
+  ['ษ'] = 'K',
+  ['ศ'] = 'L',
+  ['ซ'] = ';',
+  ['ฉ'] = 'C',
+  ['ฮ'] = 'V',
+  ['ฺ'] = 'B',
+  ['์'] = 'N',
+  ['ฒ'] = '<',
+  ['ฬ'] = '>',
+  ['ฦ'] = '?',
+}
+
+for thai, eng in pairs(thai_mappings) do
+  vim.keymap.set('', thai, eng)
+end
+
+-- Special mappings
+vim.keymap.set('', 'เเ', 'gg')
+
+-- Scrolling relative to cursor
+vim.keymap.set('', 'ผผ', 'zz')
+vim.keymap.set('', 'ผะ', 'zt')
+vim.keymap.set('', 'ผิ', 'zb')
+
+-- Window commands
+vim.keymap.set('', '<c-w>แ', '<c-w>c')
+
+-- Additional mappings
+vim.keymap.set('', '<A-S-J>', '<C-d>zz')
+vim.keymap.set('', '<A-S-K>', '<C-u>zz')
+vim.keymap.set('', '<A-S-๋>', '<C-d>')
+vim.keymap.set('', '<A-S-ษ>', '<C-u>')
