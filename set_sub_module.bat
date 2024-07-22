6207a648c6c379bdd5557025507e6fdd357a0e95
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-22 15:53:34 +0700

    Create set_sub_module.bat

---
+++ set_sub_module.bat
@@ -0,0 +1,15 @@
+
+
+echo Create Nvim Submodule
+call:del_then_symlink "%LocalAppData%\nvim" "%cd%"
+
+
+:del_then_symlink
+@REM echo.try delete "%~1"
+rd /q /s "%~1" 2>nul
+@REM echo."%~1" Deleted
+mklink /D "%~1" "%~2"
+@REM echo.Use "%~2" to Create symlink
+echo ============================================================
+echo:
+goto:eof
