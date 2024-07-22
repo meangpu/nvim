09f99c0b062f32d4e59a908e3087da1ec5711814
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-22 17:52:20 +0700

    Update set_sub_module.bat

--- set_sub_module.bat
+++ set_sub_module.bat
@@ -3,10 +3,15 @@ cd /d %~dp0
 echo Create Nvim Symlink
 
 call:create_symlink "%LocalAppData%\nvim" "%cd%"
+goto:eof
 
 :create_symlink
 echo Removing existing directory if present...
 rd /q /s "%~1" 2>nul
+
+echo Ensuring parent directory exists...
+for %%I in ("%~1") do set "parent=%%~dpI"
+if not exist "%parent%" mkdir "%parent%"
 
 echo Creating symlink...
 mklink /D "%~1" "%~2"
