c99a0ed9a54508f166d3f29e0f9d98306cb8c24d
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-23 22:50:05 +0700

    update plugin

--- init.lua
+++ init.lua
@@ -156,11 +156,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
 --
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
 --  To update plugins you can run
 --    :Lazy update
 --
@@ -182,7 +177,6 @@ require('lazy').setup({
     'easymotion/vim-easymotion',
     config = function()
       vim.api.nvim_set_keymap('n', '<Leader>e', '<Plug>(easymotion-e)', {})
-      vim.api.nvim_set_keymap('n', '<Leader>w', '<Plug>(easymotion-w)', {})
       vim.api.nvim_set_keymap('n', '<Leader>b', '<Plug>(easymotion-b)', {})
     end,
   },
@@ -821,12 +815,13 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
