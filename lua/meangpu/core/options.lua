2e5ce0d68ce2c35ee4caa46e05be7e5819116dd6
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-24 16:29:43 +0700

    refactor code

---
+++ lua/meangpu/core/options.lua
@@ -0,0 +1,32 @@
+vim.cmd 'let g:netrw_liststyle = 3'
+
+-- use ':source %' to refresh change
+local opt = vim.opt
+
+opt.clipboard = 'unnamedplus'
+opt.number = true -- show default line number
+opt.mouse = 'a'
+opt.showmode = false -- don't show normal insert visual because it already have in editor
+opt.breakindent = true
+opt.undofile = true
+opt.wrap = true
+opt.autoindent = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = 'yes'
+opt.updatetime = 250
+opt.timeoutlen = 300 -- Displays which-key popup sooner
+opt.splitright = true
+opt.splitbelow = true
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.inccommand = 'split'
+opt.cursorline = true
+opt.scrolloff = 20 -- Minimal number of screen lines to keep above and below the cursor.
+
+opt.guifont = 'Fira Code:h18'
+opt.textwidth = 100
+
+opt.hlsearch = true
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
