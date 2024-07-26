fa16b200f23524dd78924dd5eb10db27f475124f
meangpu <51825872+meangpu@users.noreply.github.com>
2024-07-26 21:39:19 +0700

    update color

--- lua/meangpu/plugins/colorscheme.lua
+++ lua/meangpu/plugins/colorscheme.lua
@@ -2,17 +2,46 @@ return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'folke/tokyonight.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
+
+  config = function()
+    local transparent = false -- set to true if you would like to enable transparency
+
+    local bg = '#000'
+    local bg_dark = '#080808'
+    local bg_highlight = '#143652'
+    local bg_search = '#0A64AC'
+    local bg_visual = '#275378'
+    local fg = '#CBE0F0'
+    local fg_dark = '#B4D0E9'
+    local fg_gutter = '#627E97'
+    local border = '#547998'
+
+    require('tokyonight').setup {
+      style = 'night',
+      transparent = transparent,
+      styles = {
+        sidebars = transparent and 'transparent' or 'dark',
+        floats = transparent and 'transparent' or 'dark',
+      },
+      on_colors = function(colors)
+        colors.bg = bg
+        colors.bg_dark = transparent and colors.none or bg_dark
+        colors.bg_float = transparent and colors.none or bg_dark
+        colors.bg_highlight = bg_highlight
+        colors.bg_popup = bg_dark
+        colors.bg_search = bg_search
+        colors.bg_sidebar = transparent and colors.none or bg_dark
+        colors.bg_statusline = transparent and colors.none or bg_dark
+        colors.bg_visual = bg_visual
+        colors.border = border
+        colors.fg = fg
+        colors.fg_dark = fg_dark
+        colors.fg_float = fg
+        colors.fg_gutter = fg_gutter
+        colors.fg_sidebar = fg_dark
+      end,
+    }
+
     vim.cmd.colorscheme 'tokyonight-night'
-    vim.cmd.hi 'Comment gui=none'
-    config = function()
-      local bg_dark = '#011628'
-      local bg_highlight = '#011628'
-      local bg_search = '#011628'
-      local bg_visual = '#011628'
-      local fg_dark = '#011628'
-      local fg_gutter = '#011628'
-      local border = '#011628'
-    end
   end,
 }
